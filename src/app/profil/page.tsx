7f85333a75c294c65b18cee4a444a767aa21080e
unknown <farrel.hunter85@gmail.com>
2025-05-03 07:53:15 +0700

    Adding Profile

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -1,13 +1,33 @@
-'use client';
-import DisplayData from '@/components/MQTTDisplayData2';
-import React from 'react';
+import { auth } from "@/auth"; // Pastikan ini sesuai struktur proyekmu
+import React from "react";
 
+export default async function Profil() {
+  const session = await auth();
 
-export default function Profil() {
+  if (!session || !session.user) {
+    return (
+      <div className="p-4 text-center text-red-500">Anda belum login.</div>
+    );
+  }
+
+  const { name, role } = session.user;
+
   return (
-      <div>
-        <DisplayData />
+    <div className="p-8 max-w-2xl mx-auto  rounded-xl shadow-md   h-screen space-y-6">
+      <h1 className="text-3xl font-bold text-center text-primary">
+        Profil Akun
+      </h1>
+      <div className="my-3 flex justify-between">
+        <div className="text-xl">
+          <span className="font-semibold">Nama:</span> {name}
+        </div>
+        <div className="text-xl">
+          <span className="font-semibold">Peran:</span> {role}
+        </div>
+        <div className="text-xl">
+          <span className="font-semibold">Model Booster:</span> {"Belum diset"}
+        </div>
       </div>
-
+    </div>
   );
 }
